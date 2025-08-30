b9d4aaeb5b2ee2739288bf4cc28b3b41bebfd79a
Ha Le <halntit@gmail.com>
2025-08-31 00:33:12 +0700

    day 5

---
+++ day05/ex02-methods-are-func/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/**
+ * Here's Abs written as a regular function with no change in functionality.
+ */
+type Vertex struct {
+	X, Y float64
+}
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(Abs(v))
+}
