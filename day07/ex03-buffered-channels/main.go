75ece602f6e1261b179eee361ffd1c1f1ac1d1ab
Ha Le <halntit@gmail.com>
2025-09-10 19:11:22 +0700

    day 7

---
+++ day07/ex03-buffered-channels/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(ch)
+	ch <- 2
+	ch <- 2 // error, buffer is full
+}
